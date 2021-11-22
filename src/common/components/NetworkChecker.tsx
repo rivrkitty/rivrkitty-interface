fb30252879fa7283a1c1b2300cd6ef5cf8974599
rivrkitty <rivrkitty@protonmail.com>
2021-11-22 20:23:02 +0100

    Added better handling incorrect network and wallet not connected cases

--- src/common/components/NetworkChecker.tsx
+++ src/common/components/NetworkChecker.tsx
@@ -1,22 +1,47 @@
-import { Paper, Typography } from "@mui/material";
+import { Button, Paper, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import { useSnackbar } from "notistack";
 import React, { ReactElement } from "react";
 import { useTranslation } from "react-i18next";
 import { defaultContentPadding } from "../../utils/theme";
 import { isValidNetworkId } from "../../web3/getNetworkData";
+import { networkSetup } from "../../web3/networkSetup";
+import { Web3ModalProvider } from "../contexts/Web3ModalProvider";
 import { useConnectWallet } from "../redux/connectWallet";
 
 export default function NetworkChecker(props: {
   children: ReactElement | undefined;
 }) {
   const { children } = props;
-  const { networkId } = useConnectWallet();
+
+  const web3Modal = React.useContext(Web3ModalProvider);
+  const { networkId, connectWallet } = useConnectWallet();
+  const { enqueueSnackbar } = useSnackbar();
   const { t } = useTranslation();
+
+  const connect = React.useCallback(
+    () => web3Modal && connectWallet(web3Modal),
+    [web3Modal, connectWallet]
+  );
 
   const isValid = networkId ? isValidNetworkId(networkId) : null;
 
-  if (isValid === false) {
+  const targetNetworkSetup = React.useCallback(() => {
+    networkSetup(1285)
+      .then(() => {})
+      .catch((e) => {
+        if (typeof e === "object" && typeof e.message === "string") {
+          enqueueSnackbar(e.message, { variant: "error" });
+        } else if (typeof e === "string") {
+          enqueueSnackbar(e, { variant: "error" });
+        } else {
+          enqueueSnackbar(t("networkUnknownError"), { variant: "error" });
+        }
+      });
+  }, [t, enqueueSnackbar]);
+
+  if (!isValid) {
     return (
       <Paper
         sx={{
@@ -40,9 +65,31 @@ export default function NetworkChecker(props: {
               justifyContent: "center",
               alignItems: "center",
               minHeight: 180,
+              flexDirection: "column",
             }}
           >
-            <Typography variant="body1">{t("wrongNetworkInfo")}</Typography>
+            <Typography variant="body1">
+              {isValid === null
+                ? t("connectWalletInfo")
+                : t("wrongNetworkInfo")}
+            </Typography>
+            {isValid === null ? (
+              <Button
+                sx={{ marginTop: 2 }}
+                variant="outlined"
+                onClick={connect}
+              >
+                {t("walletConnect")}
+              </Button>
+            ) : (
+              <Button
+                sx={{ marginTop: 2 }}
+                variant="outlined"
+                onClick={targetNetworkSetup}
+              >
+                {t("switchToMoonriver")}
+              </Button>
+            )}
           </Grid>
         </Grid>
       </Paper>
--- src/common/components/WalletButton.tsx
+++ src/common/components/WalletButton.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Web3Modal from "web3modal";
 import makeStyles from "@mui/styles/makeStyles";
 import Hidden from "@mui/material/Hidden";
 import Avatar from "@mui/material/Avatar";
 import { useTranslation } from "react-i18next";
-import { createWeb3Modal } from "../../web3/createWeb3Modal";
 import { useDisconnectWallet, useConnectWallet } from "../redux/selectors";
 import RoundedButton from "./RoundedButton";
+import { Web3ModalProvider } from "../contexts/Web3ModalProvider";
 const { renderIcon } = require("@download/blockies");
 
 const useStyles = makeStyles({
@@ -19,9 +18,9 @@ export default function WalletButton() {
   const { disconnectWallet } = useDisconnectWallet();
   const [shortAddress, setShortAddress] = React.useState("");
   const [dataUrl, setDataUrl] = React.useState<string | null>(null);
-  const [web3Modal, setModal] = React.useState<Web3Modal | null>(null);
   const { connectWallet, web3, address, connected } = useConnectWallet();
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const web3Modal = React.useContext(Web3ModalProvider);
 
   const connectWalletCallback = React.useCallback(() => {
     web3Modal && connectWallet(web3Modal);
@@ -30,10 +29,6 @@ export default function WalletButton() {
   const disconnectWalletCallback = React.useCallback(() => {
     web3 && web3Modal && disconnectWallet(web3, web3Modal);
   }, [web3, web3Modal, disconnectWallet]);
-
-  React.useEffect(() => {
-    setModal(createWeb3Modal());
-  }, [setModal]);
 
   React.useEffect(() => {
     if (web3Modal && (web3Modal.cachedProvider || window.ethereum)) {
